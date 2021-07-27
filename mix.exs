6215b693ce3bfe0d6a4caebd16ac3d036f13c3b4
Michael Davis <mcarsondavis@gmail.com>
2021-07-27 09:05:23 -0500

    add compile task for nif

--- mix.exs
+++ mix.exs
@@ -1,9 +1,28 @@
+defmodule Mix.Tasks.Compile.TreeSitterNif do
+  require Logger
+
+  def run(_args) do
+    {stdout, exit_code} =
+      System.cmd("make", ["priv/tree_sitter_nif.so"], stderr_to_stdout: true)
+
+    if exit_code == 0 do
+      :ok
+    else
+      Logger.error("""
+      'make priv/tree_sitter_nif.so' failed with exit code #{exit_code}:
+      #{stdout}
+      """)
+    end
+  end
+end
+
 defmodule Yix.MixProject do
   use Mix.Project
 
   def project do
     [
       app: :yix,
+      compilers: [:tree_sitter_nif | Mix.compilers()],
       version: "0.1.0",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
